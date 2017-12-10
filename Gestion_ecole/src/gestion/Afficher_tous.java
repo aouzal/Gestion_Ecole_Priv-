d05415702afd52d2e34fd21710972fd203cf72df
zakariaaouzal@gmail.com <zakariaaouzal@gmail.com>
2017-12-10 19:36:52 +0000

    -Connection with database
    -add a new class for testing show function
    
    -add button "tous"

--- Gestion_ecole/src/gestion/Afficher_tous.java
+++ Gestion_ecole/src/gestion/Afficher_tous.java
@@ -6,10 +6,8 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-import javax.swing.JMenuBar;
-import javax.swing.JMenu;
 
-public class Acceuil extends JFrame {
+public class Afficher_tous extends JFrame {
 
 	private JPanel contentPane;
 
@@ -20,35 +18,21 @@ public class Acceuil extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					Acceuil frame = new Acceuil();
+					Afficher_tous frame = new Afficher_tous();
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
 			}
 		});
-		
-		connectDB bdd=new connectDB();
 	}
 
 	/**
 	 * Create the frame.
 	 */
-	public Acceuil() {
+	public Afficher_tous() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
-		
-		JMenuBar menuBar = new JMenuBar();
-		setJMenuBar(menuBar);
-		
-		JMenu mnListe = new JMenu("liste");
-		menuBar.add(mnListe);
-		
-		JMenu mnRechercher = new JMenu("Rechercher");
-		mnListe.add(mnRechercher);
-		
-		JMenu mnAfficher = new JMenu("Afficher");
-		mnListe.add(mnAfficher);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
--- Gestion_ecole/src/gestion/connectDB.java
+++ Gestion_ecole/src/gestion/connectDB.java
@@ -1,5 +1,25 @@
 package gestion;
-
+import java.sql.*;
 public class connectDB {
+	private Connection con;
+	private PreparedStatement st;
+	private ResultSet rs;
+	
+	
+	
+	public connectDB() {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			
+			con=DriverManager.getConnection("jdbc:mysql://localhost/mydb","root","");
+			System.out.println("done");
+			//st=con.createStatement();
+		}catch(Exception e) {
+			
+			System.out.println("error"+ e);
+		}
+		
+		
+	}
 
 }
